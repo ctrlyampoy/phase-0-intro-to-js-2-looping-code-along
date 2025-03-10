1ddb88ea621530e3e8673814cc101761c36b7591
Yampoy <yampoy.elvis@gmail.com>
2025-03-10 16:52:08 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+// Function to return an array of thank you messages
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  // Function to count down from a given number to zero, logging each number
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  // Make sure to export the functions for testing
+  module.exports = {
+    writeCards,
+    countDown,
+  };
+  
